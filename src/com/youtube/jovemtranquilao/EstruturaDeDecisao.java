11a49cf630b556679e6840139d350e9cfda36d0c
Khai221 <juliocnarciso@hotmail.com>
2024-09-23 15:25:28 -0400

    Estruturas condicionais

---
+++ src/com/youtube/jovemtranquilao/EstruturaDeDecisao.java
@@ -0,0 +1,15 @@
+package com.youtube.jovemtranquilao;
+
+public class EstruturaDeDecisao {
+    public static void main(String[] args) {
+        Integer numero = 2;
+
+        if(numero % 2 == 0){
+            // executo codigo daqui quando verdadeiro
+            System.out.println("par");
+        }else{
+            // executo aqui quando falso
+            System.out.println("impar");
+        }
+    }
+}
