2a4085efd0c2178e111ae3b67828c6401a13bc6c
Khai221 <juliocnarciso@hotmail.com>
2024-09-24 12:34:38 -0400

    try-catch + if-else pegando expected exception com a classe Date

--- src/com/youtube/jovemtranquilao/EstruturaDeDecisao.java
+++ src/com/youtube/jovemtranquilao/EstruturaDeDecisao.java
@@ -1,14 +1,20 @@
 package com.youtube.jovemtranquilao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class EstruturaDeDecisao {
 
 
     public static void main(String[] args) {
 //        stringIgual();
-        stringComeca();
+//        stringComeca();
+        datas();
 
     }
 
+    // ESTRUTURAS CONDICIONAIS
 
     //        Integer numero = 2;
     //
@@ -33,12 +39,30 @@ public class EstruturaDeDecisao {
     //        }
     //    }
 
-    private static void stringComeca(){
-        String stringComecaCom = "a lorem";
-        if(stringComecaCom.startsWith("a")){
-            System.out.println("A frase começa com a");
-        } else {
-            System.out.println("A frase não começa com a");
+    //    private static void stringComeca(){
+    //        String stringComecaCom = "a lorem";
+    //        if(stringComecaCom.startsWith("a")){
+    //            System.out.println("A frase começa com a");
+    //        } else {
+    //            System.out.println("A frase não começa com a");
+    //        }
+    //    }
+
+    private static void datas(){
+        try {
+            Date data = new SimpleDateFormat("dd/MM/yyyy").parse("24/09/2024");
+            Date hoje = new Date();
+
+            if(data.before(new Date())){
+                System.out.println("Essa data já passou");
+            } else if(data.equals(hoje)){
+                System.out.println("Essa data condiz com o dia de hoje");
+            } else {
+                System.out.println("Essa data ainda não chegou");
+            }
+        } catch(ParseException e) {
+            System.err.println("Erro ao analisar a data: " + e.getMessage());
         }
+
     }
 }
