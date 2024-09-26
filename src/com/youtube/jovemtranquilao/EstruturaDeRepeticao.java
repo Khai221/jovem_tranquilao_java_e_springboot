a9a4a5e2bf8f174a5b02a70f5b9c6ce1a43258c9
Khai221 <juliocnarciso@hotmail.com>
2024-09-25 21:53:12 -0400

    for-loop classico com .size() e arraylist

--- src/com/youtube/jovemtranquilao/EstruturaDeRepeticao.java
+++ src/com/youtube/jovemtranquilao/EstruturaDeRepeticao.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 
 public class EstruturaDeRepeticao {
     public static void main(String[] args) {
+        listadePalavras();
+    }
+
+    public static void listadePalavras() {
         ArrayList<String> palavraList = new ArrayList<>();
 
         palavraList.add("Chuva");
@@ -15,5 +19,22 @@ public class EstruturaDeRepeticao {
             System.out.println(palavra);
         }
 
+
+        palavraList.forEach(palavra -> System.out.println(palavra));
+    }
+
+    public static void forClassico(){
+        ArrayList<String> palavraList = new ArrayList<>();
+
+        palavraList.add("Chuva");
+        palavraList.add("Vento");
+        palavraList.add("Sol");
+
+        for(int i = 0; i < palavraList.size(); i++) { // for clássico
+            System.out.println(palavraList.get(i));
+
+            // nesse for clássico, é usado um método size(), que são usados em coleções como List, Set e Map e outras classes que implementam a interface Collection.
+            // o length não é usado nesse caso pois ele retorna um número de elementos no array. e pra strings ele retorna o número de caracteres.
+        }
     }
 }
