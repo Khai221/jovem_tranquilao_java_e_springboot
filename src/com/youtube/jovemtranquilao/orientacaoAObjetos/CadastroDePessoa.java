61b84da70f45dc7d462af248e4493fea9f759c09
Khai221 <juliocnarciso@hotmail.com>
2024-09-29 18:57:47 -0400

    orientação a objetos

--- src/com/youtube/jovemtranquilao/orientacaoAObjetos/CadastroDePessoa.java
+++ src/com/youtube/jovemtranquilao/orientacaoAObjetos/CadastroDePessoa.java
@@ -2,23 +2,44 @@ package com.youtube.jovemtranquilao.orientacaoAObjetos;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class CadastroDePessoa {
-    public static void main(String[] args) {
-        Pessoa julio = new Pessoa();
-        julio.setNome("Júlio");
+    ArrayList<Pessoa> pessoaList = new ArrayList<>();
+    Maternidade maternidade = new Maternidade();
 
-        try {
-            julio.setDataDeNascimento(new SimpleDateFormat("dd/MM/yyyy").parse("11/11/2002"));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws ParseException {
+        new CadastroDePessoa().rodar();
+
+    }
+
+    private void rodar() throws ParseException {
+        Pessoa julio = maternidade.nascer("Júlio", "Masculino");
+        Pessoa maria = maternidade.nascer("Mária", "Feminino");
+        Pessoa gabriel = maternidade.nascer("Gabriel", "Masculino");
+        Pessoa robo = maternidade.nascer("Robo Android", "Robo");
+
+        pessoaList.add(julio);
+        pessoaList.add(maria);
+        pessoaList.add(gabriel);
+        pessoaList.add(robo);
+
+        ArrayList<Pessoa> homemList = new ArrayList<>();
+        ArrayList<Pessoa> mulherList = new ArrayList<>();
+        ArrayList<Pessoa> outrosGeneros = new ArrayList<>();
+
+        for(Pessoa pessoa : pessoaList){
+            if(pessoa.getSexo().equals("Masculino")) {
+                homemList.add(pessoa);
+            } else if(pessoa.getSexo().equals("Feminino")){
+                mulherList.add(pessoa);
+            } else {
+                outrosGeneros.add(pessoa);
+            }
         }
 
-        julio.setSexo("Masculino");
-        julio.mudarSexo("Feminino", "Júlia");
-        julio.casar("Narciso", "Oestreich");
-        System.out.println(julio);
+        System.out.println("");
     }
 
 }
